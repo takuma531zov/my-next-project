92dcf85f1c7b871157c2a04b2d2fe3d2dac7c439
takuma531zov <takumalshikura531@gmail.com>
2025-05-21 18:12:19 +0900

    7章まで完了

--- app/_components/Article/index.tsx
+++ app/_components/Article/index.tsx
@@ -1,6 +1,7 @@
+import Link from "next/link";
 import Image from "next/image";
 import type { News } from "@/app/_libs/microcms";
-import Date from "../Date";
+import DateComponent from "../Date";
 import Category from "../Category";
 import styles from "./index.module.css";
 
@@ -14,8 +15,13 @@ export default function Article({ data }: Props) {
 			<h1 className={styles.title}>{data.title}</h1>
 			<p className={styles.description}>{data.description}</p>
 			<div className={styles.meta}>
-				<Category category={data.category} />
-				<Date date={data.publishedAt ?? data.createdAt} />
+				<Link
+					href={`/news/category/${data.category.id}`}
+					className={styles.categoryLink}
+				>
+					<Category category={data.category} />
+				</Link>
+				<DateComponent date={data.publishedAt || data.createdAt} />
 			</div>
 			{data.thumbnail && (
 				<Image
