9dd87b6e2c02baa5891cdc587a245bdaf7e448f8
takuma531zov <takumalshikura531@gmail.com>
2025-05-23 06:27:52 +0900

    7章まで完了2

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -4,20 +4,16 @@ import Article from "@/app/_components/Article";
 import ButtonLink from "@/app/_components/ButtonLink";
 import styles from "./page.module.css";
 
-type Props = {
-	params: {
-		slug: string;
-	};
-	searchParams: {
-		dk?: string;
-	};
-};
-
-export default async function Page({ params, searchParams }: Props) {
+export default async function Page({
+	params,
+	searchParams,
+}: {
+	params: { slug: string };
+	searchParams: { dk?: string };
+}) {
 	const data = await getNewsDetail(params.slug, {
 		draftKey: searchParams.dk,
 	}).catch(notFound);
-
 	return (
 		<>
 			<Article data={data} />
