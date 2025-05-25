24fe61f80e9240b91d62c3c85d33d4803b48d3e3
takuma531zov <takumalshikura531@gmail.com>
2025-05-26 08:13:52 +0900

    8章まで完了

--- middleware.ts
+++
@@ -1,8 +0,0 @@
-import { createNextAuthMiddleware } from "nextjs-basic-auth-middleware";
-
-export const middleware = createNextAuthMiddleware();
-
-export const config = {
-	matcher: ["/(.*)"],
-};
-// This middleware will protect all routes in the application.
